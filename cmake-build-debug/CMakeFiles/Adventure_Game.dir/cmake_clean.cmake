2bf23ab225f074b81ffa5eab744aad0ffc9fc565
Alex <astalter@bridgew.edu>
2019-09-12 14:42:44 -0400

    Creation of project

---
+++ cmake-build-debug/CMakeFiles/Adventure_Game.dir/cmake_clean.cmake
@@ -0,0 +1,12 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/Adventure_Game.dir/main.cpp.obj"
+  "Adventure_Game.pdb"
+  "Adventure_Game.exe"
+  "Adventure_Game.exe.manifest"
+  "libAdventure_Game.dll.a"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/Adventure_Game.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
---
+++ main.h
@@ -0,0 +1,8 @@
+//
+// Created by scoot on 9/12/2019.
+//
+
+#ifndef ADVENTURE_GAME_MAIN_H
+#define ADVENTURE_GAME_MAIN_H
+
+#endif //ADVENTURE_GAME_MAIN_H
